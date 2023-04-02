6dad6483a644d2886cc2d11711c532abb985cf3a
she0930she <she0930she@hotmail.com>
2023-04-02 14:24:33 -0700

    servlet webxml practice

---
+++ Batch65_servlet_webxml/src/main/java/com/cogent/servlets/MyServlet.java
@@ -0,0 +1,18 @@
+package com.cogent.servlets;
+
+
+//A servlet is annotated with only the URL pattern
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/processForm")
+public class MyServlet extends HttpServlet {
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+       throws IOException {
+   response.getWriter().println("Amanda Welcome back");
+}
+}
